99cc722e62385f331adc6b100e24b0eb9d9fbf39
simontaosim <xsqfeather@gmail.com>
2017-11-23 22:25:12 +0800

    准备再换系统

--- www/static/src/layouts/AppNavBar.js
+++ www/static/src/layouts/AppNavBar.js
@@ -1,37 +1,43 @@
 import React from 'react'
-import { NavBar, Icon, TabBar } from 'antd-mobile';
-import { connect } from 'react-redux';
-import { appInfo } from '../map_props.js';
+import {Icon, NavBar} from 'antd-mobile';
+import {connect} from 'react-redux';
+import {appInfo} from '../map_props.js';
 
 class AppNavBar extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       isBack: false,
-      
+
       backTo: '/',
+      hiddenBar: false,
     }
   }
 
   renderLeft(){
+    const {history} = this.props;
     if (this.state.isBack) {
         return <Icon type="left" />;
+    }
+    const pathname = history.location.pathname;
+
+    if (pathname === '/') {
+      return <span>地理位置</span>
     }
   }
 
 
   render(){
 
-
+    const top = this.state.hiddenBar ? "-42px" : "0";
     return(
-      <div style={{ position: 'fixed', width: '100%', top: 0 }}>
+      <div style={{ position: 'fixed', width: '100%', top: top }}>
         <NavBar
             mode="light"
-            icon={this.renderLeft()}
-            onLeftClick={() => {
 
-              console.log('onLeftClick')
-            }}
+            leftContent={
+              this.renderLeft()
+            }
             rightContent={[
               <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
               <Icon key="1" type="ellipsis" />,
--- www/static/src/reducers/index.js
+++ www/static/src/reducers/index.js
@@ -2,7 +2,15 @@ import {SET_APP_TIILE} from '../actions/app.js'
 
 export function AppInfo(state={
   title: "万人车汇",
-  path: "/"
+  path: "/",
+  location: {
+    planet: "地球",
+    country: '中国',
+    province: "北京市",
+    city: '北京市',
+    rectangle: '116.0119343,39.66127144;116.7829835,40.2164962',
+    adcode: '110000',
+  }
 },action){
   switch (action.type) {
     case SET_APP_TIILE:
