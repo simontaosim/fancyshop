04c803cb3596c7b3d3c4e6ff49a9dcca5288ef76
Hecdi <1652777847@qq.com>
2018-01-13 19:17:01 +0800

    recommend组件的修改

--- www/static/src/routes/home/recommend.js
+++ www/static/src/routes/home/recommend.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import { Flex, WhiteSpace, WingBlank} from 'antd-mobile';
+import { Flex, WhiteSpace, WingBlank, Badge} from 'antd-mobile';
+import style from './recommend.css';
 import goodsImg from './one.jpg';
+import carImg from './car.xcf';
 
 
 class Recommend extends React.Component {
@@ -12,8 +14,8 @@ class Recommend extends React.Component {
   render(){
     return(
       <div  style = {{backgroundColor:'#fff',padding:'10px'}}>
-        <div >
-          <span style = {{borderLeft:'2px solid #aaa',paddingLeft:'10px',display:'block'}}>火爆推荐</span>
+        {/* <div >
+          <span style = {{borderLeft:'4px solid #aaa',paddingLeft:'10px',display:'block'}}>火爆推荐</span>
         </div>
       <WhiteSpace size="sm" />
       <Flex >
@@ -24,7 +26,10 @@ class Recommend extends React.Component {
           <span　style = {{color:'#0e80d2'}}>佣金：2％</span>
         </Flex.Item>
         <Flex.Item align="center">
-          <WingBlank size = "lg"><img src = {goodsImg} alt = "商品图片" style = {{width:"100%",height:"100%"}}/></WingBlank>
+          <WingBlank size = "lg">
+
+          <img src = {goodsImg} alt = "商品图片" style = {{width:"100%",height:"100%"}}/></WingBlank>
+
           <span style = {{color:'red'}}>现价：￥330</span><br/>
           <span　style = {{color:'#0e80d2'}}>佣金：2％</span>
         </Flex.Item>
@@ -34,7 +39,41 @@ class Recommend extends React.Component {
           <span style = {{color:'red'}}>现价：￥330</span><br/>
           <span　style = {{color:'#0e80d2'}}>佣金：2％</span>
         </Flex.Item>
-      </Flex>
+      </Flex> */}
+
+      <div className = {style['recommend-frame']}>
+        <span className = {style['recommend-font']}>火爆推荐</span>
+        <Flex justify = "between">
+          <div className = {style['test']}>
+            <img src = {goodsImg} alt = "商品图片" className = {style['good-img']}/>
+            <span className = {style['price-now']}>现价：￥330</span>
+            <span　className = {style['reward']}>佣金：2％</span>
+          </div>
+          <div className = {style['test']}>
+            <img src = {goodsImg} alt = "商品图片" className = {style['good-img']}/>
+            <span className = {style['price-now']}>现价：￥330</span>
+            <span　className = {style['reward']}>佣金：2％</span>
+          </div>
+          <div className = {style['test']}>
+            <img src = {goodsImg} alt = "商品图片" className = {style['good-img']}/>
+            <span className = {style['price-now']}>现价：￥330</span>
+            <span　className = {style['reward']}>佣金：2％</span>
+          </div>
+          {/* <div>
+            <Badge text={'爆'} style={{ marginLeft: 55 }} />
+            <img src = {goodsImg} alt = "商品图片" className = {style['good-img']}/><br/>
+            <span className = {style['price-now']}>现价：￥330</span><br/>
+            <span　className = {style['reward']}>佣金：2％</span>
+          </div>
+
+          <div>
+            <img src = {goodsImg} alt = "商品图片" className = {style['good-img']}/><br/>
+            <span className = {style['price-now']}>现价：￥330</span><br/>
+            <span　className = {style['reward']}>佣金：2％</span>
+          </div> */}
+
+        </Flex>
+      </div>
     </div>
     )
   }
