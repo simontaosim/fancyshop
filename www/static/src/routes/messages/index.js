2516b4368169d6fbf3d10e27bf15d79e9769f07c
= <274173368@qq.com>
2018-02-03 10:17:01 +0800

    读取balance数据

--- www/static/src/routes/messages/index.js
+++ www/static/src/routes/messages/index.js
@@ -14,15 +14,32 @@ import {setAppTitle} from '../../actions/app.js';
 import  AwardDetail  from './AwardDetail.js';
 import AwardIncome from './AwardIncome.js';
 import AwardHead from './AwardHead.js';
+import { asteroid } from '../../config/asteroid.config'
 
 class MessageBox extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      username: [
+        {name: 'Hiark'},
+      ],
+      balanck: [],
+    }
   }
+
+  componentDidMount() {
+    asteroid.subscribe("get.current.balance");
+    asteroid.ddp.on("added", ({collection, id, fields}) => {
+        console.log(`Element added to collection ${collection}`);
+        console.log(id);
+        console.log(fields);
+    });
+  }
+
   render(){
     return (
     <div>
-      <WingBlank/>
+      {/* <WingBlank/> */}
       <AwardHead/>
       <AwardIncome/>
       <AwardDetail/>
