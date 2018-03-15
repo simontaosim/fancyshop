3a99570b6571aa051564e7977f88467aa067b1da
simontaosim <xsqfeather@gmail.com>
2018-03-16 04:07:04 +0800

    完成区块链图片上传

--- www/static/src/routes/my/index.js
+++ www/static/src/routes/my/index.js
@@ -18,11 +18,32 @@ import { getStore} from '../../config/mUtils.js';
 import { getUserbyId,getUserbyName} from '../../actions/users'; 
 
 const alert = Modal.alert;
+const hostName = "139.198.12.188";
+const hostPort = "5002";
+const ipfs = window.IpfsApi({host: hostName, port: hostPort, protocol: 'http'});
+
+let saveImageOnIpfs = (reader) => {
+  return new Promise(function(resolve, reject) {
+    const buffer = Buffer.from(reader.result);
+    ipfs.add(buffer).then((response) => {
+      console.log(response)
+      resolve(response[0].hash);
+    }).catch((err) => {
+      console.error(err)
+      reject(err);
+    })
+  })
+}
 
 class AppMy extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      userImg,
+    }
     this.confirmWindow = this.confirmWindow.bind(this);
+    this.userImgChange = this.userImgChange.bind(this);
+    
     
   }
 
@@ -73,16 +94,36 @@ class AppMy extends React.Component{
     }
   }
 
+  userImgChange(){
+    let file =  this.refs.userImgInput.files[0];
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = (e) => {
+      console.log(reader);
+      // 上传数据到IPFS
+      saveImageOnIpfs(reader).then((hash) => {
+        console.log(hash);
+        this.setState({userImg: "http://"+hostName+":8630"+"/ipfs/" + hash})
+      });
+    }
+  }
+
 
   render(){
     
     const { currentUser } =  this.props
-   
+    const {userImg} = this.state;
     return (
       <div >
         <div className = {style['back-color']}>
           <Flex justify = 'center' align = "center">
               <img alt="" src = {userImg} className = {style['user-img']}/>
+              <input type='file' ref="userImgInput" onChange={this.userImgChange} style={{
+                    opacity: 0,
+                    position: "absolute",
+                    left: "41%",
+                    height: "59px"
+              }}/>
           </Flex>
           <Flex justify = "end" className = {style['pencil-position']} >
             <Link to = "/personal">
