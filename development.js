455669b402a374e3497445c5796308a24406d0b8
simontaosim <xsqfeather@gmail.com>
2018-03-16 01:57:30 +0800

    ipfs server

--- development.js
+++ development.js
@@ -2,7 +2,8 @@ const Application = require('thinkjs');
 const babel = require('think-babel');
 const watcher = require('think-watcher');
 const notifier = require('node-notifier');
-
+require('ipfs');
+const series = require('async-series');
 const instance = new Application({
   ROOT_PATH: __dirname,
   watcher: watcher,
@@ -14,3 +15,102 @@ const instance = new Application({
 });
 
 instance.run();
+
+//启动ipfs节点
+const IPFSFactory = require('ipfsd-ctl')
+const f = IPFSFactory.create({type: 'js'})
+const HOST_IP ="192.168.43.228";
+
+f.spawn(
+      {
+        repoPath:  __dirname+'/www/ipfs/db',
+        disposable: false,
+        init: true,
+        start: false,
+        config: {
+          "API": {
+            "HTTPHeaders": {
+              "Access-Control-Allow-Origin": [
+                "*"
+              ],
+              "Access-Control-Allow-Methods": [
+                "PUT",
+                "POST",
+                "GET"
+              ]
+            }
+          },
+          "Addresses": {
+            "Swarm": [
+              "/ip4/192.168.43.228/tcp/4403"
+            ],
+            "API": "/ip4/192.168.43.228/tcp/5002",
+            "Gateway": "/ip4/192.168.43.228/tcp/8787"
+          },
+          "Discovery": {
+            "MDNS": {
+              "Enabled": true,
+              "Interval": 10
+            },
+            "webRTCStar": {
+              "Enabled": true
+            }
+          }
+        }
+        
+      }, function (err, ipfsd) {
+        if (err) { throw err };
+        series([
+           
+            (cb) => ipfsd.cleanup((err)=>{
+              console.log("清除数据库",err);
+              console.log('清除',ipfsd.repoPath);
+              
+              cb();
+            }),
+           
+            (cb) => ipfsd.init( (err, db) => {
+                console.log(err);
+                console.log("初始化",ipfsd.repoPath);
+                console.log("初始化的返回",db);
+                cb();
+              }),
+            
+          
+            (cb) => ipfsd.getConfig('', (err, config) => {
+              console.log(JSON.stringify(config));
+              console.log(ipfsd.started)
+              if(ipfsd.started === false){
+                ipfsd.start(["ok"], (err, ipfsd) => {
+                  console.log('config then start',ipfsd);
+                  if(ipfsd === undefined){
+                    return cb();
+                  }
+                  ipfsd.id((err, id)=>{
+                    console.log(err);
+                    console.log(id);
+                    cb();
+                  })
+                 
+                })
+              }else{
+                cb();
+              }
+
+             
+            }),
+           
+            (cb) => console.log("the end config ipfsd")
+            
+            
+            
+            
+        ])
+       
+        
+        
+        
+        
+     
+  
+})
